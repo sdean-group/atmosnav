63c24fde18c0f52b8f5247817dcb990cd1cdfbde
Myles Pasetsky <myles.pasetsky@gmail.com>
2024-06-21 17:37:19 -0400

    Add docstrings to each interface/class
    Co-authored-by: Janna (Jiawei) Lin <JLjw8@users.noreply.github.com>

--- atmosnav/agents/agent.py
+++ atmosnav/agents/agent.py
@@ -10,17 +10,40 @@ from ..jaxtree import JaxTree
 # StepType = tuple[Array, Controller, Dynamics]
 
 class Agent(JaxTree):
-    """ """
+    """ 
+    An agent represents any system affected by wind that is governed by dynamics and given inputs
+    through actions.
+
+    An action is defined by what the controller expects, which converts those actions into control inputs.
+    Dynamics objects take control inputs and convert them into a change in agent state, which allows for
+    the agent to be updated. 
+    """
     
     def __init__(self, state: Array, controller: Controller, dynamics: Dynamics):
-        """ """
-        super().__init__()
+        """ Creates an agent defined by a controller and dynamics function at an initial state. 
+        
+        Args:
+           state (jax.Array): an array representing the state, often a 1D vector
+           controller (Controller): the controller of the agent
+           dynamcis (Dynamics): the dynamics of the agent
+
+        """
         self.state = state
         self.controller = controller
         self.dynamics = dynamics
 
 
     def step(self, time: jnp.float32, action: Array, wind_vector: Array) -> Self:
+        """ Returns an updated agent at the next time step. 
+        
+        Args:
+            time (jnp.float32): time of the system
+            action (jax.Array): the action to take as a vector
+            wind_vector (jax.Array): some wind disturbance as a vector
+
+        Returns:
+            (Agent): the updated agent
+        """
         control_input, controller = self.controller.action_to_control_input(time, self.state, action)
         delta_state, dynamics = self.dynamics.control_input_to_delta_state(time, self.state, control_input, wind_vector)
         return Agent(self.state + delta_state, controller, dynamics)
--- atmosnav/agents/dynamics/dynamics.py
+++ atmosnav/agents/dynamics/dynamics.py
@@ -9,8 +9,25 @@ from jax import Array
 import jax
 
 class Dynamics(JaxTree):
-    """ """
+    """ 
+    Computes a change in state of an agent based on a control input and wind vector. 
+    
+    Allows agents to be governed by physically-based dynamics.
+    """
 
     @abc.abstractmethod
     def control_input_to_delta_state(self, time: jnp.float32, state: Array, control_input: Array, wind_vector: Array) -> tuple[Array, Self]:
-        """ """
+        """
+        Computes the change of the agent's state by taking in low-level control inputs 
+
+        Args:
+            time (jnp.float32): The time now since the simulation has started
+            state (jax.Array): The state of the agent, often a one-dimensional jnp array
+            control_input (jax.Array): The control input to the agent, often a one-dimensional jnp array
+            wind_vector (jax.Array): The wind field
+
+        Returns:
+            (as a tuple: )
+            delta_state (jax.Array): The change of state,
+            Self (Dynamics): The updated Dynamics object.
+        """
