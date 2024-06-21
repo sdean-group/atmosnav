Fix nan calculation in pure agent test
Co-authored-by: Janna (Jiawei) Lin <JLjw8@users.noreply.github.com>

scripts/pure_agent_test.py
@@ -11,12 +11,9 @@ use with an agent.
 """
 
 class PureController(Controller):
-    
-    def __init__(self):
-        super().__init__()
 
     def action_to_control_input(self, time: jnp.float64, state: Array, action: Array) -> tuple[Array, Self]:
-        return (action * time) / jnp.linalg.norm(state), self
+        return (action * time * jnp.linalg.norm(state)), self
     
     def tree_flatten(self):
         return tuple(), {}
@@ -28,7 +25,7 @@ class PureController(Controller):
 class PureDynamics(Dynamics):
 
     def control_input_to_delta_state(self, time: jnp.float64, state: Array, control_input: Array, wind_vector: Array) -> tuple[Array, Self]:
-        return control_input / time, self
+        return jnp.sin(time) + control_input / (time+1), self
     
     def tree_flatten(self):
         return tuple(), {}
@@ -43,7 +40,7 @@ agent = Agent(jnp.array([ 0.0, 0.0, 0.0 ]), PureController(), PureDynamics())
 @jax.jit
 def fast_run(agent):
     def inner_loop(i, agent):
-        jax.debug.print("{x}",x=i)
+        jax.debug.print("{x}, {y}, {z}",x=agent.state[0], y=agent.state[1], z=agent.state[2])
         return agent.step(time=i, action=jnp.array([ 0.0 ]), wind_vector=jnp.array([0.0]))
     return jax.lax.fori_loop(0, 1000, inner_loop, init_val=agent)
 